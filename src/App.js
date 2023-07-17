50f8f85e4569f96622531d704d93378adcb9ddda
PeuStone <pedrohmotaaa@gmail.com>
2023-07-17 10:57:01 -0300

    Adicionando a captura de dados do cadastro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+  const novoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario colaboradorAdicionado={colaborador => novoColaboradorAdicionado(colaborador)} />
       
     </div>
   );

254ad8cd0c0c752c9b2625da0156c42b935ff7d0
PeuStone <pedrohmotaaa@gmail.com>
2023-07-18 10:43:48 -0300

    Criação do card de colaborador e estilização

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='http://github.com/viniciosneves.png' alt='Peu Stone' />
+            </div>
+
+            <div className='rodape'>
+                <h4>Vinicios Neves</h4>
+                <h5>Instrutor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
